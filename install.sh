d9bbc3c90e7c95c876abb995e7a67dbfcf7a5fe8
cuyu <icyarm@qq.com>
2017-07-07 17:43:00 +0800

    Add questions in install.sh

--- install.sh
+++ install.sh
@@ -15,7 +15,17 @@ do
     cat $f >> ~/.zshrc
 done
 
+echo "Installation almost done."
+echo "Here are some questions you should answer (you may skip and configure them manually by press 'enter' directly):"
+echo "Please input your api key for weather: "
+read token
+echo "" >> ~/.zshrc
+echo "THUNDERBOLT100K_WEATHER_TOKEN=$token" >> ~/.zshrc
+echo "Please input the city of your location (e.g. Shanghai): "
+read city
+echo "THUNDERBOLT100K_WEATHER_CITY=$city" >> ~/.zshrc
+
 echo "THUNDERBOLT100K is installed successfully"
 echo "You may still need to edit the 'POWERLEVEL9K_LEFT_PROMPT_ELEMENTS' or 'POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS' in
  '~/.zshrc' file to enable the widgets."
-echo "Also don't forget to set necessary configuration for THUNDERBOLT100K (Pls refer to https://github.com/cuyu/thunderbolt100k#configuration)"
+echo "If you want to set configurations for THUNDERBOLT100K, please refer to https://github.com/cuyu/thunderbolt100k#configuration"
