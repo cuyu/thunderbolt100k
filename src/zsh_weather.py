69556874c016114c6fe029d0e25a6dce4be08476
cuyu <icyarm@qq.com>
2017-07-07 18:19:31 +0800

    Use the forecast weather info instead of current weather

--- src/zsh_weather.py
+++ src/zsh_weather.py
@@ -2,6 +2,7 @@ import requests
 import constants
 import json
 from libs.compare_time import compare_time
+import datetime
 
 SESSION = requests.session()
 
@@ -10,29 +11,49 @@ def get_weather_info():
     assert 'WEATHER_TOKEN' in constants.CONFIG, 'You must set `THUNDERBOLT_WEATHER_TOKEN` in `~/.zshrc` file'
     assert 'WEATHER_CITY' in constants.CONFIG, 'You must set `THUNDERBOLT_WEATHER_CITY` in `~/.zshrc` file'
     response = SESSION.get(
-        'https://api.apixu.com/v1/current.json?key={token}&q={city}'.format(token=constants.CONFIG.get('WEATHER_TOKEN'),
-                                                                            city=constants.CONFIG.get('WEATHER_CITY')))
+        'https://api.apixu.com/v1/forecast.json?key={token}&q={city}&days=2'.format(
+            token=constants.CONFIG.get('WEATHER_TOKEN'),
+            city=constants.CONFIG.get('WEATHER_CITY')))
     return response.content
 
 
 def read_weather_info():
     path = constants.DATA_PATH_FORMAT.format('weather')
     with file(path, 'r') as f:
-        info = json.load(f)
-    temp = info['current']['temp_c']
-    condition = info['current']['condition']['text']
-    update_time = info['current']['last_updated']
+        content = json.load(f)
+    now = datetime.datetime.now()
+    # Use the next day's forecast info if the time is later than 23:00
+    if now.hour >= 23:
+        info = content['forecast']['forecastday'][1]['hour'][now.hour - 23]
+    else:
+        info = content['forecast']['forecastday'][0]['hour'][now.hour + 1]
+
+    temp = info['temp_c']
+    condition = info['condition']['text']
+    update_time = content['current']['last_updated']
     delta_time = compare_time(update_time, '%Y-%m-%d %H:%M')
 
-    if condition == 'rain':
+    if 'thunder' in condition:
+        symbol = "\uf0e7"
+        symbol_color = '%F{yellow}'
+    elif 'rain' in condition:
         symbol_color = '%F{blue}'
-        symbol = "\uf043"
-    elif condition == 'cloudy':
+        symbol = "\ue239"
+    elif 'Cloudy' in condition:
         symbol = "\uf0c2"
+        symbol_color = '%F{white}'
+    elif 'Overcast' in condition:
+        symbol = "\uf0c2"
+        symbol_color = '%F{grey}'
+    elif "Partly cloudy" in condition:
+        symbol = "\e21d"
         symbol_color = '%F{white}'
     elif condition == 'Sunny':
         symbol = "\uf185"
         symbol_color = '%F{yellow}'
+    elif 'snow' in condition:
+        symbol = "\uf2dc"
+        symbol_color = '%F{white}'
     else:
         symbol_color = "%F{green}"
         symbol = "\uf2c7"
